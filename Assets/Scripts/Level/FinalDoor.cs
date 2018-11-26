c1816d0c9cb1db3a16505e7e89c5945298242112
Lucas Bonnal <lubonc@gigl.polymtl.ca>
2018-11-26 14:50:38 -0500

    Niveau mieux

--- Assets/Scripts/Level/FinalDoor.cs
+++ Assets/Scripts/Level/FinalDoor.cs
@@ -8,7 +8,7 @@ public class FinalDoor : MonoBehaviour {
     Score score;
 
     void Start () {
-        door = GetComponentInChildren<Door>();
+        door = GetComponent<Door>();
         score = FindObjectOfType<Score>();
     }
 	
