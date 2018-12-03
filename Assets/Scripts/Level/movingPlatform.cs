03a7a8300e09906fe6246465d82b3a36e2969bed
myongkan <myongkan@insa-rennes.fr>
2018-12-02 19:48:44 -0500

    Basic structure of the level

---
+++ Assets/Scripts/Level/movingPlatform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingPlatform : MonoBehaviour {
+
+    [SerializeField]
+    float distance = 10.0f;
+    [SerializeField]
+    float speed = 0.5f;
+
+    int direction = 1;
+    Rigidbody _Rb;
+
+    // Use this for initialization
+    void Start () {
+        _Rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        _Rb.MovePosition(new Vector3(0.0f, 0.0f, Time.deltaTime * speed));
+	}
+}
