02f6618f9b9450646721466e19cffdce90fb2911
Laora Heintz <laora.heintz@gmail.com>
2018-12-03 13:05:12 -0500

    final

---
+++ Assets/EndGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour {
+
+    [SerializeField]
+    GameObject winCanvas;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+            End();
+    }
+
+    private void End()
+    {
+        winCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
